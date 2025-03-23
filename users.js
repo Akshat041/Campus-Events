597f12d879f995839a097c3f4e6eff7aeec9e72a
Akshat Singh <132469517+Akshat041@users.noreply.github.com>
2025-03-24 00:03:20 +0530

    added users module.

---
+++ users.js
@@ -0,0 +1,15 @@
+// Users data (for authentication)
+const users = [
+  {
+    username: "prof_kit",
+    password: "password123", // In real app, use proper password hashing
+    role: "faculty",
+  },
+  {
+    username: "student_kit",
+    password: "password123",
+    role: "student",
+  },
+];
+
+export { users };
